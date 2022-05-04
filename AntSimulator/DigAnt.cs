41d0ab3bc1bbbd4bac591c35043a7ac4457f87ec
Alex McCune <alexmccune1224@gmail.com>
2022-05-03 19:29:19 -0700

    Currently working on SimpleAnt that impliments BaseAnt abstract class and IMoveSelector interface

---
+++ AntSimulator/DigAnt.cs
@@ -0,0 +1,11 @@
+namespace AntSimulator
+{
+    sealed class DigAnt : AbstractAnt
+    {
+        public const char symbol = 'D';
+        public override void Act(Tile currentTile)
+        {
+            return;
+        }
+    }
+}
---
+++ AntSimulator/FlyAnt.cs
@@ -0,0 +1,11 @@
+namespace AntSimulator
+{
+    sealed class FlyAnt : AbstractAnt
+    {
+        public const char symbol = 'F';
+        public override void Act(Tile currentTile)
+        {
+            return;
+        }
+    }
+}
