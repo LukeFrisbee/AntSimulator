dc58ef89a55f9446629e24afc6b8be2ff6e57a7b
lukebfrisbee@gmail.com <lukebfrisbee@gmail.com>
2022-05-03 18:14:34 -0700

    Tile object created.

---
+++ AntSimulator/Tile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntSimulator
+{
+    internal class Tile
+    {
+        public int x;
+        public int y;
+
+        public bool dirt;
+        public bool air;
+
+        public int foodCount = 0;
+
+        public bool hasAnt;
+    }
+}
