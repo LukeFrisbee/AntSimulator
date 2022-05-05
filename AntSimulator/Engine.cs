4fb7e57f1a3150165afa878c5e37ef098158b75b
Alex McCune <alexmccune1224@gmail.com>
2022-05-04 18:02:31 -0700

    Improved RNG. First third of the game pure random. Remainder more logical RNG food placement.

--- AntSimulator/Engine.cs
+++ AntSimulator/Engine.cs
@@ -6,15 +6,17 @@ namespace AntSimulator // Note: actual namespace depends on the project name.
     {
         private Grid grid = new Grid();
 
-        private int tickCount;
-        private int delay;
+        private double initialTickCount;  
+        private double tickCount;
+        private double delay;
 
         public int foodCount = 10;
 
-        public Engine(int tickCount = 1000, int delay = 250)
+        public Engine(double tickCount = 1000, double delay = 250)
         {
             this.tickCount = tickCount;
             this.delay = delay;
+            this.initialTickCount = tickCount;
         }
 
         Tile GenerateFood()
@@ -23,30 +25,78 @@ namespace AntSimulator // Note: actual namespace depends on the project name.
 
             int terrainSelection = randy.Next(0, 10);
 
-            //30% chance to spawn food in the air
-            if (terrainSelection <= 2)
-            {
-                int randomX = randy.Next(1, grid.Width-1);
-                int randomY = randy.Next(grid.Height/2 + 1, grid.Height-1);
+            /*For the initial part of the simulation food spawning in dirt doesn't matter,
+            //but to stop dig ants from devouring the entire underground we need to stop putting food under the ground
+            */
 
-                Tile food = grid.grid[randomY, randomX];
-                food.foodCount = foodCount;
-                grid.foods.Add(food);
-                return food;
+            //first third of the game, generate total random food spots
+            if (tickCount / initialTickCount >= .66)
+            {
+                //30% chance to spawn food in the air
+                if (terrainSelection <= 2)
+                {
+                    int randomX = randy.Next(1, grid.Width - 1);
+                    int randomY = randy.Next(1, grid.Height - 1);
+
+                    Tile food = grid.grid[randomY, randomX];
+                    food.foodCount = foodCount;
+                    grid.foods.Add(food);
+                    return food;
+                }
+                //spawn anywhere else instead
+                else
+                {
+                    int randomX = randy.Next(1, grid.Width - 1);
+                    int randomY = randy.Next(grid.Height/2, grid.Height - 1);
+
+                    Tile food = grid.grid[randomY, randomX];
+                    food.foodCount = foodCount;
+                    grid.foods.Add(food);
+                    return food;
+                }
             }
-            //Remainder percentage should be on dirt
+
+
+            //remainder of the game need to carefully place random food tiles on non-dirt blocks
             else
             {
-                int randomX = randy.Next(1, grid.Width-1);
-                int randomY = randy.Next(1, grid.Height-1);
+                List<Tile> nonDirtTiles = new List<Tile>();
+                foreach (Tile tile in grid.grid)
+                {
+                    if (tile.State != TileState.Dirt && tile.State != TileState.Wall && tile.x != grid.Width && tile.y != grid.Height) { 
+                        nonDirtTiles.Add(tile); 
+                    }
+                }
+                //20% chance the tile in the sky
+                if (terrainSelection <= 2)
+                {
+                    int randomNonDirtTileIndex = randy.Next(nonDirtTiles.Count);
 
-                Tile food = grid.grid[randomY, randomX];
-                food.foodCount = foodCount;
-                grid.foods.Add(food);
-                return food;
+                    while (nonDirtTiles[randomNonDirtTileIndex].State != TileState.Air){
+                        randomNonDirtTileIndex = randy.Next(nonDirtTiles.Count); 
+                    }
+                
+                    Tile food = grid.grid[nonDirtTiles[randomNonDirtTileIndex].y, nonDirtTiles[randomNonDirtTileIndex].x];
+                    food.foodCount = foodCount;
+                    grid.foods.Add(food);
+                    return food;
+                }
+                //put the tile on a normal non-dirt tile
+                else { 
+                    int randomNonDirtTileIndex = randy.Next(nonDirtTiles.Count);
+
+                    while (nonDirtTiles[randomNonDirtTileIndex].State != TileState.Dirt && nonDirtTiles[randomNonDirtTileIndex].State != TileState.Air){
+                        randomNonDirtTileIndex = randy.Next(nonDirtTiles.Count); 
+                    }
+                    Tile food = grid.grid[nonDirtTiles[randomNonDirtTileIndex].y, nonDirtTiles[randomNonDirtTileIndex].x];
+                    food.foodCount = foodCount;
+                    grid.foods.Add(food);
+                    return food;
+
+                }
+
             }
 
-            //random x and y values within the border, will be one less than the sides of the screen
         }
 
 
@@ -55,8 +105,8 @@ namespace AntSimulator // Note: actual namespace depends on the project name.
             Random randy = new Random();
             grid.DrawGrid();
 
-            QueenAnt qua = new QueenAnt(1, grid.Height/2, grid);
-            TrailAnt timmy = new TrailAnt(10, grid.Height/2, grid, qua);
+            QueenAnt qua = new QueenAnt(1, grid.Height / 2, grid);
+            TrailAnt timmy = new TrailAnt(10, grid.Height / 2, grid, qua);
             FlyingAnt flik = new FlyingAnt(2, grid.Height/2, grid, qua);
             DiggingAnt doug = new DiggingAnt(35, grid.Height/2, grid, qua);
             
@@ -72,7 +122,7 @@ namespace AntSimulator // Note: actual namespace depends on the project name.
                 {
                     //why round down when I can just type cast to int haha
                     //for (int i = 0; i < (int)(ants.Count * 1.5); i++)
-                    for (int i = 0; i < 5; i++)
+                    for (int i = 0; i < 2; i++)
                     {
                         tiles.Add(GenerateFood());
                     }
@@ -82,7 +132,7 @@ namespace AntSimulator // Note: actual namespace depends on the project name.
                     tiles.UnionWith(grid.ants[i].Act());
                 grid.DrawTiles(tiles);
 
-                Thread.Sleep(delay);
+                Thread.Sleep((int)delay);
 
                 tickCount--;
             }
